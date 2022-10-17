d990f52269970ebbc6445cd4ab361effe0c4ef9c
CiRA <cira.ami@gmail.com>
2022-10-17 16:19:57 +0000

    Update boostrap.sh

--- boostrap.sh
+++ boostrap.sh
@@ -13,3 +13,10 @@ mkdir -p /root/.cira_core_install/share
 unzip -qq d.zip && cp -r d /root/.cira_core_install/share
 
 echo -e """\e[1;32m******  ********\e[1m"""
+
+# instaall ros
+sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+apt-get update
+apt-get install ros-melodic-ros-base -y
+
